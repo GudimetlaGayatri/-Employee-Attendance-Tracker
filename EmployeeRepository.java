86332af4e24590a7aa26eeeaf7291ce0a1f664ae
GudimetlaGayatri <105493236+GudimetlaGayatri@users.noreply.github.com>
2025-06-03 13:20:36 +0530

    Create EmployeeRepository.java

---
+++ EmployeeRepository.java
@@ -0,0 +1,8 @@
+// EmployeeRepository.java
+package com.example.attendance.repository;
+
+import com.example.attendance.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
+
